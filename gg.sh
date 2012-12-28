6c154390df43c9cd697ff37f56e4f80209a083cc
root <root@vimal.rms>
2012-12-28 07:19:52 +0300

    new commit

---
+++ gg.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# check daemon
+A= "rsync"
+B= ps -a | grep rsync| cut -d " " -f 7
+if [ A != B ]
+then
+rsync -avz /  alexsmith@alexsmith.strongspace.com:/strongspace/alexsmith/public
+else
+echo "hai"
+
+fi
